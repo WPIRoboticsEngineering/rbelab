8193fbed55208b3807c5fb08a65255b76ebbce2b
Keshuai Xu <urillx@gmail.com>
2017-12-04 14:27:14 -0500

    Create install_turtlebot_pkgs.sh

---
+++ install_turtlebot_pkgs.sh
@@ -0,0 +1,1 @@
+sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs
